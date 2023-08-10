bac5af9a71e357ba6f3dbf95f66a8db0230de6c3
seobinbang7 <newmso5382@gmail.com>
2023-08-11 07:31:01 +0900

    초기 세팅

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Practice from './pages/Practice/Practice';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Practice />
+      </>
+    </div>
+  );
+}
+
+export default App;
